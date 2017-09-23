4a3c92c2eee00b5a45e600ab141780d170816980
Kurimoto Yumi <kurimoto@sb.ecei.tohoku.ac.jp>
2017-09-24 02:26:50 +0900

    add chapter1

---
+++ Chapter1/Assets/GameDirector.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using UnityEngine.SceneManagement;
+
+public class GameDirector : MonoBehaviour {
+
+	public GameObject seikai;
+	public int clickMove = -8;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonDown (0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit2D hit = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction, Mathf.Infinity);
+
+			if (hit.collider && clickMove <= 8) {
+				GameObject item = hit.collider.gameObject;
+				item.transform.position = new Vector3(clickMove, -3, 0);
+				clickMove += 4;
+			}
+		}
+			
+		if (clickMove > 8) {
+			if (GameObject.FindWithTag ("body").transform.position.x == -8 &&
+			    GameObject.FindWithTag ("cell").transform.position.x == -4 &&
+			    GameObject.FindWithTag ("nucleus").transform.position.x == 0 &&
+			    GameObject.FindWithTag ("chromosome").transform.position.x == 4 &&
+			    GameObject.FindWithTag ("nucleotide").transform.position.x == 8) {
+
+				seikai.SetActive (true);
+			} else {
+				SceneManager.LoadScene (0);
+			}
+		}
+	}
+}
---
+++ Chapter1/Assets/ItemGenerator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+
+public class ItemGenerator : MonoBehaviour {
+
+	public GameObject bodyPrefab;
+	public GameObject cellPrefab;
+	public GameObject choromosomePrefab;
+	public GameObject nucleotidePrefab;
+	public GameObject nucleusPrefab;
+
+	int[] ary = new int[] { 1, 2, 3, 4, 5 };
+	float xrange = -6.0f;
+
+	// Use this for initialization
+	void Start () {
+
+//		int number = Random.Range (0, Items.Length);
+//		Instantiate(Items[number],transform.position,transform.rotation);
+
+		var random_order = ary.OrderBy(i => Guid.NewGuid()).ToArray();//配列をランダムに並べ替え
+		for (int x = 0; x <= 4; x++) {
+			GameObject item;
+			if (random_order [x] == 1) {
+				item = Instantiate(bodyPrefab) as GameObject;
+			} else if (random_order [x] == 2) {
+				item = Instantiate(cellPrefab) as GameObject;
+			} else if (random_order [x] == 3) {
+				item = Instantiate(choromosomePrefab) as GameObject;
+			} else if (random_order [x] == 4) {
+				item = Instantiate(nucleotidePrefab) as GameObject;
+			} else {
+				item = Instantiate(nucleusPrefab) as GameObject;
+			}
+
+			item.transform.position = new Vector3 (xrange, 1.5f, 0.0f);
+			xrange += 3.0f;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
